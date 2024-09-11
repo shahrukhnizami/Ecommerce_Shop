21b4550be6623cdadcddeb567c436a8d649bb720
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-09-11 05:16:03 +0500

    ss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -111,31 +111,10 @@ function Header(){
 									<ul className="account_selection">
 										<li><Link to={"signin"}><i className="fa fa-sign-in" aria-hidden="true"></i>Sign In</Link></li>
 										<li><Link to={"register"}><i className="fa fa-user-plus" aria-hidden="true"></i>Register</Link></li>
-										<li><Link to={"signup"}><i className="fa fa-user-plus" aria-hidden="true"></i>SignUp</Link></li>
-										<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+										{/* <li><Link to={"signup"}><i className="fa fa-user-plus" aria-hidden="true"></i>SignUp</Link></li> */}
+							
 
-{/* <!-- Modal --> */}
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 																				
 									</ul>
 
